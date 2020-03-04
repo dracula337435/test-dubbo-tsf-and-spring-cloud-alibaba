548669a46ed894c1199e576add4371d50030524b
dk <dracula337435@163.com>
2020-03-04 16:16:35 +0800

    dubbo方式，provider方试验成功。配置移入bootstrap.yaml。

--- provider/src/main/java/io/dracula/test/dubbo/tsf/and/alibaba/provider/EchoServiceImpl.java
+++ provider/src/main/java/io/dracula/test/dubbo/tsf/and/alibaba/provider/EchoServiceImpl.java
@@ -5,6 +5,7 @@ import io.dracula.test.dubbo.tsf.and.alibaba.api.EchoService;
 /**
  * @author dk
  */
+@org.apache.dubbo.config.annotation.Service
 public class EchoServiceImpl implements EchoService {
 
     public String echo(String msg) {
